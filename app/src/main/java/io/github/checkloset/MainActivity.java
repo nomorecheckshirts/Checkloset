a6478e62618fc546cd368c80cb73f2db65ffc01f
thohohong <so0426yeon@gmail.com>
2021-02-11 21:33:10 +0900

    make calendar

--- app/src/main/java/io/github/checkloset/MainActivity.java
+++ app/src/main/java/io/github/checkloset/MainActivity.java
@@ -3,12 +3,20 @@ package io.github.checkloset;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
 public class MainActivity extends AppCompatActivity {
+    MaterialCalendarView materialCalendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.calendar_view);
+
+        materialCalendarView = findViewById(R.id.calendarView);
+
+        materialCalendarView.setTopbarVisible(false);
     }
 }
