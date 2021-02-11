1d8d70b7975ee9bceb7c2506a51cf36ba0d25363
thohohong <so0426yeon@gmail.com>
2021-02-11 22:06:57 +0900

    Basic Frame of MainActivity(CalendarView)

--- app/src/main/java/io/github/checkloset/MainActivity.java
+++ app/src/main/java/io/github/checkloset/MainActivity.java
@@ -2,21 +2,28 @@ package io.github.checkloset;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Window;
+import android.widget.LinearLayout;
 
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
 public class MainActivity extends AppCompatActivity {
     MaterialCalendarView materialCalendarView;
+    LinearLayout container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.calendar_view);
+        setContentView(R.layout.activity_main);
 
+        container = findViewById(R.id.container);
+
+        LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.calendar_view, container, true);
         materialCalendarView = findViewById(R.id.calendarView);
-
-        materialCalendarView.setTopbarVisible(false);
+        materialCalendarView.setTopbarVisible(false); //invisible yymm of calendar
     }
 }
